e3d94b5b111a19cbeb01e16dad2b6473c79db58a
Kelvin Quarcoo <kelvinquarcoo2005@gmail.com>
2024-06-12 10:51:34 +0000

    added the 'create an account' screen with some features

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import Welcome from './screens/Welcome';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import Onboarding from './screens/Onboarding';
-// import SignIn from './screens/SignIn';
+import SignIn from './screens/SignIn';
 // import MobileLogin from './screens/MobileLogin';
 const Stack = createStackNavigator();
 
@@ -23,12 +23,12 @@ export default function App() {
           options={{
             headerShown: false,
           }} />
-        {/* <Stack.Screen 
+        <Stack.Screen 
           name="SignIn" 
           component={SignIn}
           options={{
             headerShown: false,
-          }} /> */}
+          }} />
         {/* <Stack.Screen 
           name="MobileLogin" 
           component={MobileLogin}
